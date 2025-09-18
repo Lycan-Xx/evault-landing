2329aea2e2aae9a79b1d1f22702f167c4f528cca
Lycan-Xx <msbello514@gmail.com>
2025-09-18 17:58:07 +0100

    Moving all the previous files into the new project, ang perfectly configuring it

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Mission from "./components/Mission";
+import Offer from "./components/Offer";
+import Services from "./components/Services";
+import Feedback from "./components/Feedback";
+import MobileApp from "./components/MobileApp";
+import Footer from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [language, setLanguage] = useState("English");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar language={language} setLanguage={setLanguage} />
+      <Hero language={language} />
+      <About language={language} />
+      <Mission language={language} />
+      <Offer language={language} />
+      <Services />
+      <Feedback />
+      <MobileApp />
+      <Footer setLanguage={setLanguage} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
